e21c275c7e7cd288612b6dc62251bbca1f986be9
dmcoomer1985 <dmcoomer1985@gmail.com>
2023-09-05 23:15:47 -0400

    added styling and picture to Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
-
+import image from "../Assets/pexels-tembela-bohle-1884581.jpg"
 const Home = () => {
   //Header, logo, image, link to log-in page. Link to sign-in page, styling.
   return (
     <>
-      <h2>Home Page</h2>
+    <div>
+    <img src={image} id="Clothing" alt="Clothing Picture" />
+     </div>
+    
     </>
   );
 };
